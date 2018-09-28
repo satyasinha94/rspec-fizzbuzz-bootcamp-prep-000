f11557e80494db93584edccc990017a934d98cc0
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-28 04:30:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(number)
   if number % 3 == 0
      "Fizz"
-elsif number % 5 == 0
+  elsif number % 5 == 0
     "Buzz"
-elsif number % 3 == 0 && number % 5 == 0
+  elsif number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
   end
 end
