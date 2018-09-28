f2c3a9a8adfdfb46c0c4c7b14fd3f82844885354
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-28 04:53:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if number % 3 == 0
-     "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
-  else number % 15 == 0 
-   puts "FizzBuzz"
+  if number % 3 == 0 && number % 5 == 0
+     "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  else number % 5 == 0
+   puts "Buzz"
  end
 end
